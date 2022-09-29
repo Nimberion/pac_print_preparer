f8eec0d70861b0e11f9cdb48d58a346710c6dd0e
Nimberion <philipp.volkmer@web.de>
2022-09-29 19:25:20 +0200

    pdfjs-dist added

--- src/app.vue
+++ src/app.vue
@@ -1,29 +1,79 @@
 <template>
-	<!-- <div class="p-4 text-center">
-		<h1 class="text-xl font-bold mb-4">PAC Print Preparer</h1>
-		<div class="mb-4">Ziehe die zu bearbeitende Datei in das Fenster oder wähle sie über den Button aus.</div>
-		<button class="w-32 mb-4 px-2 py-1 border border-gray-400 bg-gray-300 rounded-sm hover:bg-gray-100" @click="chooseFile">
-			Durchsuchen
-		</button>
-		<div class="text-sm mb-4">
-			Datei: <span>{{ foo }}</span>
+	<div class="text-center text-sm grid grid-rows-[28px_108px_32px] place-items-center p-4">
+		<h1 class="text-xl font-bold">PAC Print Preparer</h1>
+
+		<div v-if="fileName" class="text-sm">
+			<div>Datei:</div>
+			<div>{{ fileName }}</div>
 		</div>
-		<button class="w-32 mb-4 px-2 py-1 border border-gray-400 bg-gray-300 rounded-sm hover:bg-gray-100" @click="cleanFile">
-			Start
-		</button>
-	</div> -->
-	<Home />
+		<div v-else class="">Ziehe die zu bearbeitende Datei in das Fenster oder wähle sie über den Button aus.</div>
+		<div>
+			<button class="w-28 mx-2 px-2 py-1 border border-gray-400 bg-gray-300 rounded-sm hover:bg-gray-100" @click="selectFile">
+				Öffnen</button
+			><button class="w-28 mx-2 px-2 py-1 border border-gray-400 bg-gray-300 rounded-sm hover:bg-gray-100" @click="cleanFile">
+				Starten
+			</button>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-	import Home from "./component/Home.vue";
-	// let foo = "bar";
+	import { ref } from "vue";
+	import { open } from "@tauri-apps/api/dialog";
+	import { readBinaryFile, BaseDirectory, readDir } from "@tauri-apps/api/fs";
+	import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
+	// import * as pdfjsLib from "pdfjs-dist/build/pdf";
+	import worker from "pdfjs-dist/build/pdf.worker.entry";
+	// pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
 
-	// function chooseFile() {
-	// 	console.log("File choosen");
-	// }
+	const pdfPath = ref("test");
+	const fileName = ref("");
+	const file = ref();
+	// GlobalWorkerOptions.workerSrc = "/pdf.worker.min.js";
+	GlobalWorkerOptions.workerSrc = worker;
 
-	// function cleanFile() {
-	// 	console.log("File cleaned");
-	// }
+	async function selectFile() {
+		console.log("chose");
+		pdfPath.value = (await open({
+			multiple: false,
+			filters: [{ name: "PDF", extensions: ["pdf"] }],
+			directory: false,
+			title: "Öffnen",
+			// defaultPath: "",
+		})) as string;
+
+		// console.log(readDir(""));
+
+		console.log(pdfPath.value);
+
+		fileName.value = pdfPath.value.split("\\").at(-1);
+
+		file.value = await readBinaryFile(pdfPath.value);
+
+		getItems(file.value);
+
+		// console.log(file.value);
+		fileName.value = file.value;
+	}
+
+	async function getContent(src: Uint8Array, number: number) {
+		const doc = await getDocument(src).promise;
+		const page = await doc.getPage(number); //doc.numPages
+		return await page.getTextContent();
+	}
+
+	async function getItems(src: Uint8Array) {
+		for (let index = 0; index < 8; index++) {
+			const content = await getContent(src, index + 1);
+			// const items = content.items.map((item) => {
+			// 	console.log(item);
+			// });
+			// return items;
+			console.log(content.items);
+		}
+	}
+
+	function cleanFile() {
+		console.log("clean");
+	}
 </script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	darkMode: false,
-	content: [],
 	theme: {
 		extend: {},
 	},
